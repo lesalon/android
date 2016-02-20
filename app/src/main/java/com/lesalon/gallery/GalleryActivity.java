343351d2b7e703c0a18f988249d0bab10e20a883
dudurudh <ad3572@nyu.edu>
2016-02-20 11:48:18 -0500

    Morning commit

--- app/src/main/java/com/lesalon/gallery/GalleryActivity.java
+++ app/src/main/java/com/lesalon/gallery/GalleryActivity.java
@@ -4,8 +4,10 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.AbsListView;
+import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.QuickContactBadge;
+import android.widget.ScrollView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 
@@ -17,19 +19,6 @@ public class GalleryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
-        TableLayout table = new TableLayout(this);
-        for (int i = 0; i < mRows; i++) {
-
-            TableRow tr = new TableRow(mContext);
-
-            for (int j = 0; j < mCols; j++) {
-
-                ImageView view = new ImageView(this);
-                view.setImageResource(R.drawable.star_on);
-                tr.addView(view);
-            }
-            table.addView(tr);
-        }
 
 
     }
