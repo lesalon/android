586fdee922702e54cc6125bf46f9edcb9efeb961
Kanishk Tripathi <kanishk.tripathi87@gmail.com>
2016-02-20 04:09:35 -0500

    Added GCM features

---
+++ app/src/main/java/com/lesalon/Constants.java
@@ -0,0 +1,27 @@
+package com.lesalon;
+
+/**
+ * Created by kanishk on 2/20/16.
+ */
+public class Constants {
+    public static final String PUBLISH_KEY   = "pub-c-ca4ea028-393b-4372-a27c-058515e3066a";
+    public static final String SUBSCRIBE_KEY = "sub-c-ec2d693e-d79d-11e5-bcd9-02ee2ddab7fe";
+
+    public static final String CHAT_PREFS    = "com.lesalon.SHARED_PREFS";
+    public static final String CHAT_USERNAME = "com.lesalon.SHARED_PREFS.USERNAME";
+    public static final String CHAT_ROOM     = "com.lesalon.CHAT_ROOM";
+
+    public static final String JSON_GROUP = "groupMessage";
+    public static final String JSON_DM    = "directMessage";
+    public static final String JSON_USER  = "chatUser";
+    public static final String JSON_MSG   = "chatMsg";
+    public static final String JSON_TIME  = "chatTime";
+
+    public static final String STATE_LOGIN = "loginTime";
+
+    public static final String GCM_REG_ID    = "gcmRegId";
+    public static final String GCM_SENDER_ID = "190097877816"; // Get this from
+    public static final String GCM_POKE_FROM = "gcmPokeFrom"; // Get this from
+    public static final String GCM_CHAT_ROOM = "gcmChatRoom"; // Get this from
+    public final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+}
