4d7c73d6a2ee8b7e013158fcb4232963485f3f96
prathamesh89 <prathamesh_pp@hotmail.com>
2016-02-20 21:01:05 -0500

    Merging code

--- app/src/main/java/com/lesalon/gallery/GalleryActivity.java
+++ app/src/main/java/com/lesalon/gallery/GalleryActivity.java
@@ -3,6 +3,7 @@ package com.lesalon.gallery;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.AbsListView;
 import android.widget.GridLayout;
 import android.widget.ImageView;
@@ -10,16 +11,71 @@ import android.widget.QuickContactBadge;
 import android.widget.ScrollView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.TextView;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
 import com.lesalon.R;
 
-public class GalleryActivity extends AppCompatActivity {
+public class GalleryActivity extends AppCompatActivity implements
+        GoogleApiClient.OnConnectionFailedListener{
+
+    private GoogleApiClient mGoogleApiClient;
+    private GoogleSignInResult result;
+    private TextView mStatusTextView;
+    private TextView badges;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
 
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        mStatusTextView = (TextView) findViewById(R.id.username);
+        badges = (TextView) findViewById(R.id.badges);
+
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
+        if (opr.isDone()) {
+            // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
+            // and the GoogleSignInResult will be available instantly.
+            Log.d("Gallary Avtivity", "Got cached sign-in");
+            result = opr.get();
+
+            if (result.isSuccess()) {
+                // Signed in successfully, show authenticated UI.
+                GoogleSignInAccount acct = result.getSignInAccount();
+                String name = acct.getDisplayName();
+                String fname = name.split(" ")[0];
+                mStatusTextView.setText(acct.getDisplayName());
+                badges.setText(fname+"'s Badges");
+            }
+            //handleSignInResult(result);
+        }
+    }
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d("Gallery Activity", "onConnectionFailed:" + connectionResult);
     }
 }
